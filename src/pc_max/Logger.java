e83db42dd253853428bca8fc6338e5c3b69f1c19
Jakub <35482785+Zimoslaw@users.noreply.github.com>
2022-04-23 18:29:41 +0200

    Implementacja algorytmu zachłannego

---
+++ src/pc_max/Logger.java
@@ -0,0 +1,65 @@
+package pc_max;
+
+/**
+ * Klasa do wyświetlania wszelkich komunikatów w konsoli
+ *
+ * @author Jakub Niewiarowski
+ */
+
+public class Logger {
+
+    /**
+     * Wyświetlanie komunikatów w konsoli
+     * @param errNum numer błędu/informacji
+     * @param msg dodatkowa informacja do wyświetlenia
+     */
+    public void Log(int errNum, String msg) {
+        _Printout(errNum, msg);
+    }
+
+    /**
+     * Wyświetlanie komunikatów w konsoli
+     * @param errNum numer błędu/informacji
+     * @param msg dodatkowa informacja do wyświetlenia
+     */
+    public void Log(int errNum, int msg) {
+        _Printout(errNum, Integer.toString(msg));
+    }
+
+    /**
+     * Ostateczne wyświetlanie komunikatów w konsoli
+     * @param e numer błędu/informacji
+     * @param m dodatkowa informacja do wyświetlenia
+     */
+    private void _Printout(int e, String m) {
+        String err = null;
+
+        switch(e) {
+            //-----Błędy-----
+            case 0:
+                err = "TEST";
+                break;
+            case 1:
+                err = "Błąd pliku z problemem";
+                break;
+            //-----Info-----
+            case 10:
+                err = "Liczba procesorów";
+                break;
+            case 11:
+                err = "Liczba zadań";
+                break;
+            case 12:
+                err = "Czasy zadań";
+                break;
+            case 13:
+                err = "Wynik:";
+                break;
+            default:
+                err = "Nieznany błąd";
+                break;
+        }
+
+        System.out.println(err+": "+m);
+    }
+}
--- src/pc_max/Main.java
+++ src/pc_max/Main.java
@@ -3,9 +3,14 @@ package pc_max;
 import java.util.ArrayList;
 
 public class Main {
+    
+    static int procNum = 0; //Liczba procesorów
+    static ArrayList<Integer> tasks; //Lista czasów wszystkich zadań
+    static int Tmax = 0; //Czas wykonania ostatniego zadania
 
     public static void main(String[] args) throws Exception {
 
+<<<<<<< Updated upstream
         FileHandler fileHandler = new FileHandler();
 
         fileHandler.getProblem("C:\\Users\\Zimek\\Desktop\\test.txt");
@@ -13,7 +18,69 @@ public class Main {
         ArrayList<Integer> tasks = fileHandler.getTasks();
         for (int t:tasks) {
             System.out.println(t);
+=======
+        FileHandler fileHandler = new FileHandler(); //dane problemu
+        Logger logger = new Logger(); //komunikaty, logi
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //wczytywanie inputu
+        String filePath; //ścieżka do pliku z problemem
+        
+
+        System.out.println("Podaj ścieżkę pliku:");
+
+        filePath = reader.readLine();
+
+        if(fileHandler.getProblem(filePath)) //wczytywanie problemu
+        {
+            procNum = fileHandler.getProcessors();
+            logger.Log(10,procNum);
+
+            tasks = fileHandler.getTasks();
+            logger.Log(11,tasks.size());
+
+            /*logger.Log(12,"");
+
+            for (int t:tasks) {
+                System.out.println(t);
+            }*/
+            
+            Greedy();
+            logger.Log(13, Tmax); //Wydrukowanie wyniku
+>>>>>>> Stashed changes
         }
 
+    }
+
+    /**
+     * Algorytm zachłanny
+     */
+    private static void Greedy() {
+        Logger logger = new Logger();
+        int[][] T = new int[procNum][tasks.size()];
+            
+            int procI = 0;
+            int taskI = 0;
+            for(int t:tasks) {
+                for(int i = 0; i < tasks.size(); i++)
+                    if(T[procI][i] == 0)
+                    {
+                        T[procI][i] = t;
+                        break;
+                    } 
+                        
+                procI++;
+                if(procI == procNum) {
+                    procI = 0;
+                    taskI = 0;
+                }
+            }
+            
+            for(int i = 0; i < procNum; i++) {
+                int sum = 0;
+                for(int j = 0; j < tasks.size(); j++)
+                    sum += T[i][j];
+                logger.Log(0,sum);
+                if(sum > Tmax)
+                    Tmax = sum;
+            }
     }
 }
